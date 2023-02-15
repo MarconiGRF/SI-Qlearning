24dea677f096b66d4ef044bdc2a091a2d01e20b7
Marconi Gomes <marconi@academy.cin.ufpe.br>
2023-02-15 20:53:29 -0300

    wip(algorithm): removes the matrix modification

--- client.py
+++ client.py
@@ -1,8 +1,6 @@
 from utils.utils import Utils
-from utils.connection import connect
+from utils.connection import connect, get_state_reward
 
 #Aqui vocês irão colocar seu algoritmo de aprendizado
 if __name__ == '__main__':
     matrix = Utils.get_matrix_from_file()
-    matrix[0].jump = 0.0
-    Utils.write_matrix_to_file(matrix)
