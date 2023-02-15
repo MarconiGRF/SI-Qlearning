21d2cc6411d7a62fc0fac834e44959977c83f107
Marcelo Santana <andersonmarcelo2002@gmail.com>
2023-02-15 09:43:46 -0300

    write matrix to file

--- utils/utils.py
+++ utils/utils.py
@@ -5,7 +5,9 @@ class Utils:
     """
     Defines utils for usage on the project, making chore tasks easier.
     """
-
+    def __init__(self) -> None:
+        self.matrix = self.get_matrix_from_file()
+    
     @staticmethod
     def get_matrix_from_file():
         matrix = []
@@ -18,7 +20,18 @@ class Utils:
 
         return matrix
 
-    def write_matrix_to_file(self, matrix):
+    def write_matrix_to_file(self):
+        with open(os.path.dirname(__file__) + '/../resultado.txt') as text_file:
+            text_file.truncate(0)
+        
+        text = ''
+        for platform in self.matrix:
+            text += str(platform.left) + ' ' + str(platform.right) + ' ' + str(platform.jump) + '\n'
+        text += '\n'
+
+        with open(os.path.dirname(__file__) + '/../resultado.txt') as text_file:
+            text_file.write(text)
+
         # 1.  Apagar o arquivo inteiro
         # 2.  Pra cada item da matrix
           # 2.1 Construir a linha da forma -> "platform.left platform.right platform.jump\n"
