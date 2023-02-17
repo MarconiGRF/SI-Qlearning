0f51424585c15e173d6838f92ca6390b798d0574
Marcelo Santana <andersonmarcelo2002@gmail.com>
2023-02-17 11:38:45 -0300

    next steps

--- client.py
+++ client.py
@@ -16,5 +16,15 @@ if __name__ == '__main__':
     print('New platform is -> ' + platform)
     print('Direction is -> ' + direction)
     print('Reward from LAST platform+action is -> ' + str(reward))
-    # for element in matrix:
-    #     print(str(element))
+
+    """
+    NEXT STEPS:
+
+    1- Escolher a ação a ser feita (se valores todos iguais, escolhe uma randomly)
+    2- executar a ação
+    3 - Atualizar o valor da coombinação [(estado = plataforma + direção) + ação] ANTERIOR.
+    3.1 - Formula: (estado,ação) novo = (O valor que já existia) + alpha(arbitrario, talvez menor que 0.5) * [a recompensa retornada no passo 2 + (fator de desconto(arbitrário) * QMAX - (O valor que já existia)]
+
+    3.1.1 Se houver mudança de plataforma o QMAX vai ser o maior valor entre ação/direção da plataforma atual, se não houver mudança de plataforma ler os melhores apenas do novo estado
+    cada estado é uma linha em resultado
+    """
