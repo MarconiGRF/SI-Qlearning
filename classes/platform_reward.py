25091dddecfdbaf608a45225090842d923599a9e
Eliab Bernardino <eliab2107@gmail.com>
2023-02-24 11:00:23 -0300

    commit changes in classes

--- classes/platform_reward.py
+++ classes/platform_reward.py
@@ -1,3 +1,6 @@
+import math
+from classes.constants import Constants
+
 class PlatformReward:
 
     def __init__(self, left_reward, right_reward, jump_reward, file_line):
@@ -5,6 +8,19 @@ class PlatformReward:
         self.left = left_reward
         self.right = right_reward
         self.jump = jump_reward
+        self.direction = self.determinate_direction()
+        self.platform = math.ceil(self.line / 4)
+
+    def determinate_direction(self):
+        if (self.line-1) % 4  == 0:
+            return Constants.DIRECTION["00"]
+        elif (self.line-1) % 4 == 1:
+            return Constants.DIRECTION["01"]
+        elif (self.line-1) % 4 == 2:
+            return Constants.DIRECTION["10"]
+        else:
+            return Constants.DIRECTION["11"]
 
     def __str__(self) -> str:
-        return ('Line - ' + str(self.line) + ' Left - ' + str(self.left) + ' Right - ' + str(self.right) + ' Jump - ' + str(self.jump))
+        return 'Line - ' + str(self.line) + ' Left - ' + str(self.left) + ' Right - ' + str(self.right) + ' Jump - ' \
+               + str(self.jump)
